046f0411f524155dae9af76a9a60fc21b4e2c272
kit4er0 <112687445+kit4er0@users.noreply.github.com>
2022-11-28 20:33:37 +0700

    Update project.py

--- project.py
+++ project.py
@@ -3,18 +3,15 @@ import os
 
 pygame.init()
 
-fps = 60
 width, height = 900, 500
 screen = pygame.display.set_mode((width, height))
     
 pygame.display.set_caption("Jumpie Cat")
 
-logo = pygame.image.load("pic juppiecat/cat run up right.png")
+logo = pygame.image.load("pic juppiecat/catwallpaper.png")
 pygame.display.set_icon(logo)
 
 # เพื่มรูปภาพที่ใช้ในgame
-
-font_color = (0, 0, 0)
 
 def main():
     run = True
